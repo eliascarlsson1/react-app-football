4dff7d0d44eb73f9da373fa53c5fcdfd67570cb3
Elias Carlsson <eeliascarlsson@gmail.com>
2023-04-27 17:55:47 +0200

    backend

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)  # Add this line to enable CORS for all routes
 
 # Historical data API Route
 @app.route("/api/historical")
 def historical():
     return ["BE2223", "PL2223", "LL2223"]
+
+if __name__ == "__main__":
+    app.run(debug=True)
