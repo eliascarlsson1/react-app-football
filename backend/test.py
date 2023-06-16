c858ad6c118ca2ceb7ebe5b2533f1949c9972acf
Elias Carlsson <eeliascarlsson@gmail.com>
2023-06-16 14:55:51 +0200

    test

--- backend/test.py
+++ backend/test.py
@@ -1,22 +1,23 @@
-from src.prepare_data.prepare_data import prepare_scraped_game
+from src.prepare_data.prepare_data import prepare_scraped_game, prepare_relevant_data
 from src.data_handling.data_handling_utils import get_all_historical_data_dict
 
 
 ### Variables ###
 all_historical_data_dict = get_all_historical_data_dict()
 
-prepare_scraped_game(
-    "Chelsea",
-    "Brentford",
-    "27/04/2023",
-    "OddsOver",
-    "OddsUnder",
-    "OddsH",
-    "OddsA",
-    "OddsD",
-    "2223",
-    "PL",
-    all_df_dict=all_historical_data_dict,
-)
+# prepare_scraped_game(
+#     "Chelsea",
+#     "Brentford",
+#     "27/04/2023",
+#     "OddsOver",
+#     "OddsUnder",
+#     "OddsH",
+#     "OddsA",
+#     "OddsD",
+#     "2223",
+#     "PL",
+#     all_df_dict=all_historical_data_dict,
+# )
 
 
+prepare_relevant_data(all_df_dict=all_historical_data_dict, only_current_year=True, setStatus=print)
